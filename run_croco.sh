37b552765a70ce18ea0696c5e44d2a2e551719fa
Laurent caraffa <caraffa.laurent@gmail.com>
2020-04-08 17:47:37 +0200

    [WASURE 3D/2D OK] 3D surface ok & VISU

---
+++ run_croco.sh
@@ -0,0 +1,7 @@
+#./build//build-spark-Release-D3/bin/wasure-local-exe --output_dir /home/laurent/shared_spark/tests_outputs/run_3d_croco/ --input_dir ./datas/3d_bench/ --dim 3 --bbox -50x50:-50x50:-50x50  --pscale 2 --nb_samples 5 --mode surface --lambda 1 --step full_stack --seed 18696 --label full_stack --filename ./datas/3d_bench_small/croco_small.ply
+
+# ./build//build-spark-Release-D2/bin/wasure-local-exe --output_dir /home/laurent/shared_spark/tests_outputs/run_2d_wasure/ --input_dir ./datas/2d_austin/imgs2/ --dim 2 --bbox 0000x10000:0000x10000  --pscale 20 --nb_samples 5 --mode surface --lambda 0 --step full_stack --seed 18696 --label full_stack --filename ./datas/2d_austin/imgs2/austin2_res.ply
+
+./build//build-spark-Release-3/bin/wasure-local-exe --output_dir /home/laurent/shared_spark/tests_outputs/run_3d_croco/ --input_dir ./datas/3d_bench_small --dim 3 --bbox 0000x10000:0000x10000  --pscale 20 --nb_samples 5 --mode surface --lambda 1 --step full_stack --seed 18696 --label full_stack --filename ./datas/3d_bench/croco.ply 
+
+
