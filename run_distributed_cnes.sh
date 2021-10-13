e39801a4f0824cd6910a171b9626ea2d41bb7913
laurent <caraffa.laurent@gmail.com>
2021-10-13 09:42:17 +0200

    update cnes version

--- run_distributed_cnes.sh
+++ run_distributed_cnes.sh
@@ -10,8 +10,9 @@ function cnes_run_algo
 {
     ## WORKS!!
     # spark-shell  --master spark://${NODE_NAME}:7077 yarn --deploy-mode client --jars ${DDT_MAIN_DIR}/build/spark/target/scala-2.11/iqlib-spark_2.11-1.0.jar --conf "spark.executor.memoryOverhead=${MULTIVAC_MEMORY_OVERHEAD}"  
+    echo "SPARK_TMP_DIR =>>> $SPARK_TMP_DIR"
     echo ":load /home/ad/caraffl/code/spark-ddt/services/wasure/workflow/workflow_wasure_generic.scala"
-    spark-shell -i /home/ad/caraffl/code/spark-ddt/services/wasure/workflow/workflow_wasure_generic.scala --master spark://${NODE_NAME}:7077 yarn --deploy-mode client --jars ${DDT_MAIN_DIR}/build/spark/target/scala-2.11/iqlib-spark_2.11-1.0.jar --driver-memory 10G --executor-memory 15G --conf "spark.executor.memoryOverhead=${MULTIVAC_MEMORY_OVERHEAD}" -Dlog4j.configuration=log4j-driver.properties --files log4j-driver.properties,log4j-executor.properties  --conf spark.executor.extraJavaOptions=-Dlog4j.debug=true  --conf spark.executor.extraJavaOptions=-Dlog4j.configuration=file:./log4j-executor.properties  --conf spark.yarn.app.container.log.dir=/home/ad/caraffl/code/spark-ddt  --conf "spark.memory.offHeap.enabled=true"   --conf "spark.memory.offHeap.size=10g"
+    spark-shell -i /home/ad/caraffl/code/spark-ddt/services/wasure/workflow/workflow_wasure_generic.scala --master spark://${NODE_NAME}:7077 yarn --deploy-mode client --jars ${DDT_MAIN_DIR}/build/spark/target/scala-2.11/iqlib-spark_2.11-1.0.jar --driver-memory 10G --executor-memory 15G --conf "spark.executor.memoryOverhead=${MULTIVAC_MEMORY_OVERHEAD}" -Dlog4j.configuration=log4j-driver.properties --files log4j-driver.properties,log4j-executor.properties  --conf spark.executor.extraJavaOptions=-Dlog4j.debug=true  --conf spark.executor.extraJavaOptions=-Dlog4j.configuration=file:./log4j-executor.properties  --conf spark.yarn.app.container.log.dir=/home/ad/caraffl/code/spark-ddt  --conf "spark.memory.offHeap.enabled=true"   --conf "spark.memory.offHeap.size=10g"  --conf spark.local.dir=${SPARK_TMP_DIR}
     
     ## GOOOD
     # spark-shell  --master spark://${NODE_NAME}:7077 yarn --deploy-mode client --jars ${DDT_MAIN_DIR}/build/spark/target/scala-2.11/iqlib-spark_2.11-1.0.jar --conf "spark.executor.memoryOverhead=${MULTIVAC_MEMORY_OVERHEAD}"  --properties-file spark-defaults.conf
@@ -19,7 +20,6 @@ function cnes_run_algo
     #--conf "spark.executor.extraJavaOptions='-Dlog4j.configuration=log4j.properties'" --driver-java-options "-Dlog4j.configuration=file:/home/ad/caraffl/code/spark-ddt/log4j-cnes.properties"
     #spark-shell --master spark://${NODE_NAME}:7077
 }
-
 
 function run_cnes_church
 {
@@ -69,25 +69,23 @@ function run_cnes_toulouse_full
     cnes_run_algo
 }
 
-
-
-
 function cnes_load_spark
 {
     module load spark
 }
-
 
 # Init
 function cnes_init_spark
 {
     OUTPUT=$(qsub ${DDT_MAIN_DIR}/services/pbs/start_cluster_v2.sh)
     echo "cluster start => $OUTPUT"
+    echo "TMPDIR => $TMPDIR"
     QSUB_ID="${OUTPUT%.*}"
     echo "QSUB ID => $QSUB_ID"
     module load spark
     sleep 3
     NODE_NAME=$(qstat -f ${QSUB_ID} | grep exec_host | grep -Po  'node[0-9]*' | head -1)
+    echo $TMPDIR
     echo "Node name => $NODE_NAME"
     echo ""
     echo "/!\\ Spark context Web UI available at /!\\"
@@ -95,12 +93,12 @@ function cnes_init_spark
     echo ""
 }
 
-
 # Init
 function cnes_init_spark_v2
 {
     OUTPUT=$(qsub ${DDT_MAIN_DIR}/services/pbs/start_cluster_v2.sh)
     echo "cluster start => $OUTPUT"
+    export SPARK_TMP_DIR="/tmp/pbs.${OUTPUT}"
     QSUB_ID="${OUTPUT%.*}"
     echo "QSUB ID => $QSUB_ID"
     module load spark
@@ -142,14 +140,3 @@ function kill_spark
 {
     qdel $QSUB_ID 
 }
-
-
-# cnes_init_spark
-# cnes_load_spark
-# run_multivac_church
-
-# print stats
-
-
-# Kill job
-#qdel $QSUB_ID 
--- update_cnes.sh
+++ update_cnes.sh
@@ -1,2 +1,3 @@
-scp -r run_distributed_cnes.sh   caraffl@hal.cnes.fr:~/code/spark-ddt/
+scp -r *.sh   caraffl@hal.cnes.fr:~/code/spark-ddt/
 scp -r /home/laurent/code/spark-ddt/services/wasure/workflow/* caraffl@hal.cnes.fr:~/code/spark-ddt/services/wasure/workflow/
+scp -r /home/laurent/code/spark-ddt/services/pbs/* caraffl@hal.cnes.fr:~/code/spark-ddt/services/pbs/
