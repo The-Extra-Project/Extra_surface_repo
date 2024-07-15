0e6f1ba32142acf309e3384ff91e198f06048258
Laurent Caraffa <caraffa.laurent@gmail.com>
2024-07-15 16:44:54 +0200

    change lambda example

---
+++ run_examples.sh
@@ -0,0 +1,2 @@
+./run_workflow.sh --input_dir ${PWD}/datas/lidar_hd_crop_1/ --output_dir ${PWD}/outputs_examples/lidar_hd_crop_1/
+./run_workflow.sh --input_dir ${PWD}/datas/lidar_hd_crop_2/ --output_dir ${PWD}/outputs_examples/lidar_hd_crop_2/
