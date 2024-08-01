2052760e6920ab38c0cc9cf480059dea89baaa04
dhruvmalik007 <malikdhruv1994@gmail.com>
2024-08-01 13:15:02 +0000

    WIP: - adding experimental version for resolving : useSearch params not being wrapped in suspense
    - adding dockerignore for avoiding the reference output files
    
    - resolving the bug in global.css concerning the dark theme description (as told by charlie)

--- ui/middleware.ts
+++ ui/middleware.ts
@@ -1,9 +1,9 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from 'src/utils/supabase_auth_middleware'
+//import { updateSession } from 'src/utils/supabase_auth_middleware'
 
-export async function middleware(request: NextRequest) {
-    return await updateSession(request)
-  }
+// export async function middleware(request: NextRequest) {
+//     return await updateSession(request)
+//   }
 
 export const config = {
     matcher: [
